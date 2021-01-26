bb367ab08e908763e2cc68d4139bb2451b6ddb2d
thivakarann <73923739+thivakarann@users.noreply.github.com>
2021-01-26 12:57:33 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,37 @@
-def main():
-  print('You rolled a die')
+import random
 
-if __name__== "__main__":
-  main()
+def main():
+    player1=0
+    player1wins=0
+    player2=0
+    player2wins=0
+    rounds=1
+
+    while rounds !=4:
+        print("Rounds"+str(rounds))
+        player1 =dice_roll()
+        player2 =dice_roll()
+        print ("player 1 roll:" + str(player1))
+        print ("player 1 roll:" + str(player2))
+        if player1==player2:
+            print("Draw")
+        elif player1>player2:
+            player1wins+=1
+            print("Player 1 wins")
+        else:
+            player2wins+=1
+            print("Player 2 wins")
+        rounds +=1
+    if player1wins==player2wins:
+        print("Draw")
+    elif player1wins>player2wins:
+        print("Player 1 wins - rounds won:"+str(player1wins))
+    else:
+        print("Player 2 wins - rounds won:"+str(player2wins))
+        
+        
+def dice_roll():
+    diceRoll=random.randint(1,6)
+    return diceRoll
+
+main()
